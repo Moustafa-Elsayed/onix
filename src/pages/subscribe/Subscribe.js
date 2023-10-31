d8e7b06f48ee46d07d5f1c2ef76fcf9a1392fd96
Moustafa Elsayed <moelsayed949@gmail.com>
2023-10-31 23:03:57 +0200

    subscribe section form

--- src/pages/subscribe/Subscribe.js
+++ src/pages/subscribe/Subscribe.js
@@ -1,40 +1,41 @@
 import React from "react";
 import "./subscribe.css";
-import TextField from "@mui/material/TextField";
+import { motion } from "framer-motion";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 const Subscribe = () => {
   return (
     <div className="subscribe">
-      <img
-        src=".\images\subscribe-dec.png"
-        alt=""
-      />
+      <img src=".\images\subscribe-dec.png" alt="" />
       <h2>Know Your Website SEO Score by Email</h2>
       <Box
         sx={{
           display: "flex",
-          backgroundColor: "white",
-          width: "fit-content",
           
         }}
       >
-        <TextField
-          required
-          id="outlined-required"
-          defaultValue="Hello World"
-          sx={{ backgroundColor: "transparent", border: "none",outline:"none" }}
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          defaultValue="Hello World"
-        />
-        <Button variant="contained" color="primary">
-          Subscribe
-        </Button>
+        <form action="">
+          <input type="text" placeholder="Your Website Url" />
+          <input type="email" placeholder="Your Email" />
+        </form>
       </Box>
+      <Button
+      component={motion.div}
+      whileHover={{
+        scale: 1.1,
+        transition: { duration: 0.3 },
+      }}
+      whileTap={{ scale: 0.9 }}
+      sx={{
+        paddingTop:"10px",
+        color: "black",
+        marginLeft: "10px",
+        "&:hover": { backgroundColor: "#ff8465" },
+      }}
+      variant="contained" color="primary" >
+        Subscribe
+      </Button>
     </div>
   );
 };
