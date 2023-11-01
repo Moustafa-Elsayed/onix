c9fb498d7a6ac81d5109c33ee45a97ba298d7e11
Moustafa Elsayed <moelsayed949@gmail.com>
2023-11-02 01:24:59 +0200

    modified folder

--- src/components/cardswipper/CardSwipper.js
+++ src/components/cardswipper/CardSwipper.js
@@ -1,45 +1,45 @@
-import React from "react";
-import "./cradwipper.css";
-import { Swiper, SwiperSlide } from "swiper/react";
+// import React from "react";
+// import "./cradwipper.css";
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
-import "swiper/css/effect-cards";
+// import "swiper/css";
+// import "swiper/css/effect-cards";
 
-// import required modules
-import { EffectCards } from "swiper/modules";
-import { Autoplay } from "swiper/modules";
+// // import required modules
+// import { EffectCards } from "swiper/modules";
+// import { Autoplay } from "swiper/modules";
 
-const CardSwipper = () => {
-  return (
-    <div>
-      <Swiper
-        effect={"cards"}
-        grabCursor={true}
-        modules={[EffectCards, Autoplay]}
-        className="swipers"
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-      >
-        <SwiperSlide className="swiper-slides">
-          <img src="./images/portfolio-01.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slides">
-          <img src="./images/portfolio-02.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slides">
-          <img src="./images/portfolio-03.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slides">
-          <img src="./images/portfolio-03.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide className="swiper-slides">
-          <img src="./images/portfolio-01.jpg" alt="" />
-        </SwiperSlide>
-      </Swiper>
-    </div>
-  );
-};
+// const CardSwipper = () => {
+//   return (
+//     <div>
+//       <Swiper
+//         effect={"cards"}
+//         grabCursor={true}
+//         modules={[EffectCards, Autoplay]}
+//         className="swipers"
+//         autoplay={{
+//           delay: 2500,
+//           disableOnInteraction: false,
+//         }}
+//       >
+//         <SwiperSlide className="swiper-slides">
+//           <img src="./images/portfolio-01.jpg" alt="" />
+//         </SwiperSlide>
+//         <SwiperSlide className="swiper-slides">
+//           <img src="./images/portfolio-02.jpg" alt="" />
+//         </SwiperSlide>
+//         <SwiperSlide className="swiper-slides">
+//           <img src="./images/portfolio-03.jpg" alt="" />
+//         </SwiperSlide>
+//         <SwiperSlide className="swiper-slides">
+//           <img src="./images/portfolio-03.jpg" alt="" />
+//         </SwiperSlide>
+//         <SwiperSlide className="swiper-slides">
+//           <img src="./images/portfolio-01.jpg" alt="" />
+//         </SwiperSlide>
+//       </Swiper>
+//     </div>
+//   );
+// };
 
-export default CardSwipper;
+// export default CardSwipper;
